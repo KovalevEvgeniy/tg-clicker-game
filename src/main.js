cb4461391d01cb146549b9fecc3e0e4b6813dd1a
Evgeniy Igorevich <ametist16@gmail.com>
2024-01-18 02:35:56 +0400

    start

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+const token = '6810904236:AAEAFJXxNmgn1DbmUF-OuexNZldwTTKr_x4git init'
+
+createApp(App).mount('#app')
